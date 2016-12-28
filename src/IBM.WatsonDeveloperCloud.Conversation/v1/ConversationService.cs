fafd2da439c09895e904d1da2e7c277a63b311af
Maykon . <maykonholly@hotmail.com>
2016-12-28 16:40:53 -0200

    - Correction constructor and method Conversation - Add Integration Test Conversation - Add Unit Test Conversation - Minor adjustments

--- src/IBM.WatsonDeveloperCloud.Conversation/v1/ConversationService.cs
+++ src/IBM.WatsonDeveloperCloud.Conversation/v1/ConversationService.cs
@@ -2,13 +2,14 @@
 using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
 using IBM.WatsonDeveloperCloud.Http;
 using IBM.WatsonDeveloperCloud.Service;
+using System;
 
 namespace IBM.WatsonDeveloperCloud.Conversation.v1
 {
     public class ConversationService : WatsonService, IConversationService
     {
         const string PATH_MESSAGE = "/v1/workspaces/{0}/message";
-        const string VERSION_DATE_2016_07_11 = "2016-07-11";
+        const string VERSION_DATE_2016_09_20 = "2016-09-20";
         const string SERVICE_NAME = "conversation";
         const string URL = "https://gateway.watsonplatform.net/conversation/api";
 
@@ -19,20 +20,48 @@ namespace IBM.WatsonDeveloperCloud.Conversation.v1
                 this.Endpoint = URL;
         }
 
+        public ConversationService(string userName, string password)
+            : this()
+        {
+            if (string.IsNullOrEmpty(userName))
+                throw new ArgumentNullException(nameof(userName));
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+
+            this.SetCredential(userName, password);
+        }
+
+        public ConversationService(IClient httpClient)
+            : this()
+        {
+            if (httpClient == null)
+                throw new ArgumentNullException(nameof(httpClient));
+
+            this.Client = httpClient;
+        }
+
         public MessageResponse Message(string workspaceId, MessageRequest request)
         {
             MessageResponse result = null;
 
-            using (IClient client = new WatsonHttpClient(this.Endpoint, this.UserName, this.Password))
+            if (string.IsNullOrEmpty(workspaceId))
+                throw new ArgumentNullException("parameter: workspaceId");
+
+            try
             {
                 result =
-                    client.WithAuthentication(this.UserName, this.Password)
-                          .PostAsync(this.Endpoint + string.Format(PATH_MESSAGE, workspaceId))
-                          .WithArgument("version", VERSION_DATE_2016_07_11)
-                          .WithHeader("accept", HttpMediaType.APPLICATION_JSON)
-                          .WithBody<MessageRequest>(request, MediaTypeHeaderValue.Parse(HttpMediaType.APPLICATION_JSON))
-                          .As<MessageResponse>()
-                          .Result;
+                this.Client.WithAuthentication(this.UserName, this.Password)
+                      .PostAsync(this.Endpoint + string.Format(PATH_MESSAGE, workspaceId))
+                      .WithArgument("version", VERSION_DATE_2016_09_20)
+                      .WithHeader("accept", HttpMediaType.APPLICATION_JSON)
+                      .WithBody<MessageRequest>(request, MediaTypeHeaderValue.Parse(HttpMediaType.APPLICATION_JSON))
+                      .As<MessageResponse>()
+                      .Result;
+            }
+            catch (Exception)
+            {
+                throw;
             }
 
             return result;
---
+++ test/IBM.WatsonDeveloperCloud.Conversation.IntegrationTests/ConversationServiceIntegrationTest.cs
@@ -0,0 +1,37 @@
+﻿using IBM.WatsonDeveloperCloud.Conversation.v1;
+using IBM.WatsonDeveloperCloud.Conversation.v1.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IBM.WatsonDeveloperCloud.Conversation.IntegrationTests
+{
+    [TestClass]
+    public class ConversationServiceIntegrationTest
+    {   
+        const string WORKSPACE_ID = "0a0c06c1-8e31-4655-9067-58fcac5134fc";
+
+        [TestMethod]
+        public void Message()
+        {
+            #region messageRequest
+
+            MessageRequest messageRequest = new MessageRequest()
+            {
+                Input = new InputData()
+                {
+                    Text = "Turn on the lights"
+                }
+            };
+
+            #endregion messageRequest
+
+            ConversationService service = new ConversationService();
+            service.Endpoint = "https://watson-api-explorer.mybluemix.net/conversation/api";
+
+            var results = service.Message(WORKSPACE_ID, messageRequest);
+
+            //Assert.IsNotNull(results);
+            //Assert.IsTrue(results.Intents.Count >= 1);
+        }
+    }
+
+}

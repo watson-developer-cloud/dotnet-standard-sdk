8c60580297d714de05677191ecc9a538d20e2612
atilatosta <atila.tosta@gmail.com>
2017-02-05 11:41:01 -0200

    Personality Insights: service implementation

--- src/IBM.WatsonDeveloperCloud.PersonalityInsights/v3/IPersonalityInsightsService.cs
+++ src/IBM.WatsonDeveloperCloud.PersonalityInsights/v3/IPersonalityInsightsService.cs
@@ -4,6 +4,9 @@ namespace IBM.WatsonDeveloperCloud.PersonalityInsights.v3
 {
     public interface IPersonalityInsightsService
     {
-        Profile GetProfile();
+        /// <summary>
+        /// Generates a personality profile for the author of the input text. The service accepts a maximum of 20 MB of input content. It can analyze text in Arabic, English, Japanese, or Spanish and return its results in a variety of languages. You can provide plain text, HTML, or JSON input. The service returns output in JSON format by default, but you can request the output in CSV format.
+        /// </summary>
+        Profile GetProfile(ProfileOptions options);
     }
 }

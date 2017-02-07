233747f5da6b49c03d87664016e869ac0fb96885
Ajiemar Santiago <ajiemar@gmail.com>
2017-02-07 09:55:39 -0600

    Speech to Text service example, made RecognizeStatus data model public

--- src/IBM.WatsonDeveloperCloud.SpeechToText/v1/Models/RecognizeStatus.cs
+++ src/IBM.WatsonDeveloperCloud.SpeechToText/v1/Models/RecognizeStatus.cs
@@ -22,6 +22,6 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.v1.Models
     public class RecognizeStatus
     {
         [JsonProperty("session")]
-        SessionStatus Session { get; set; }
+        public SessionStatus Session { get; set; }
     }
 }

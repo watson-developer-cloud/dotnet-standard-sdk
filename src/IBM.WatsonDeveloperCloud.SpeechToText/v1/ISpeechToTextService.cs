38ab8a7fb28f4728f4a31bb544fd4359fa2a1ff3
atilatosta <atila.tosta@gmail.com>
2017-02-15 15:02:27 -0200

    Added Recognize and ObserveResult methods

--- src/IBM.WatsonDeveloperCloud.SpeechToText/v1/ISpeechToTextService.cs
+++ src/IBM.WatsonDeveloperCloud.SpeechToText/v1/ISpeechToTextService.cs
@@ -76,5 +76,26 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.v1
         /// <param name="options">The audio to be transcribed in the format specified by the contentType parameter.</param>
         /// <returns>Returns SpeechRecognitionEvent object that contains the results that are provided in a JSON SpeechRecognitionEvent object. The response includes one or more instances of the object depending on the value of the interimResults parameter.</returns>
         SpeechRecognitionEvent Recognize(RecognizeOptions options);
+
+        /// <summary>
+        /// Sends audio and returns transcription results for a session-based recognition request. By default, returns only the final transcription results for the request. To see interim results, set the parameter interim_results to true in a call to the <see cref="ObserveResult(ObserveResultOptions)"/>  result method. 
+        /// The service imposes a data size limit of 100 MB per session. It automatically detects the endianness of the incoming audio and, for audio that includes multiple channels, downmixes the audio to one-channel mono during transcoding. The request must pass the cookie that was returned by the <see cref="CreateSession(string)" /> method.
+        /// You specify the parameters of the request as a path parameter, request headers, and query parameters. You provide the audio as the body of the request. This method is preferred to the multipart approach for submitting a session-based recognition request.
+        /// For requests to transcribe live audio as it becomes available, you must set the Transfer-Encoding header to chunked to use streaming mode. In streaming mode, the server closes the session (response code 408) if the service receives no data chunk for 30 seconds and the service has no audio to transcribe for 30 seconds. The server also closes the session (response code 400) if no speech is detected for inactivity_timeout seconds of audio (not processing time); use the inactivity_timeout parameter to change the default of 30 seconds.
+        /// To enable polling by the observe_result method for large audio requests, specify an integer with the sequence_id query parameter.
+        /// </summary>
+        /// <param name="sessionId">The identifier of the session to be used.</param>
+        /// <param name="options">The audio to be transcribed in the format specified by the contentType parameter.</param>
+        /// <returns>Returns SpeechRecognitionEvent object that contains the results that are provided in a JSON SpeechRecognitionEvent object. The response includes one or more instances of the object depending on the value of the interimResults parameter.</returns>
+        SpeechRecognitionEvent Recognize(string sessionId, RecognizeOptions options);
+
+        /// <summary>
+        /// Requests results for a recognition task within a specified session. You can submit this method multiple times for the same recognition task. To see interim results, set the interim_results parameter to true. The request must pass the cookie that was returned by the <see cref="ISpeechToTextService.CreateSession(string)" /> method.
+        /// To see results for a specific recognition task, specify a sequence ID (with the sequence_id query parameter) that matches the sequence ID of the recognition request. A request with a sequence ID can arrive before, during, or after the matching recognition request, but it must arrive no later than 30 seconds after the recognition completes to avoid a session timeout (response code 408). Send multiple requests for the sequence ID with a maximum gap of 30 seconds to avoid the timeout.
+        /// Omit the sequence ID to observe results for an ongoing recognition task. If no recognition task is ongoing, the method returns results for the next recognition task regardless of whether it specifies a sequence ID.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns>Returns one or more instances of a <see cref="SpeechRecognitionEvent" /> object depending on the input and the value of the interim_results paramete</returns>
+        List<SpeechRecognitionEvent> ObserveResult(ObserveResultOptions options);
     }
 }
--- test/IBM.WatsonDeveloperCloud.SpeechToText.UnitTests/SpeechToTextServiceUnitTest.cs
+++ test/IBM.WatsonDeveloperCloud.SpeechToText.UnitTests/SpeechToTextServiceUnitTest.cs
@@ -23,6 +23,9 @@ using IBM.WatsonDeveloperCloud.SpeechToText.v1;
 using IBM.WatsonDeveloperCloud.SpeechToText.v1.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using IBM.WatsonDeveloperCloud.Http.Exceptions;
+using System.Net.Http;
+using System.Net;
 
 namespace IBM.WatsonDeveloperCloud.SpeechToText.UnitTest
 {
@@ -71,6 +74,45 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.UnitTest
             Assert.IsNotNull(models);
             client.Received().GetAsync(Arg.Any<string>());
             Assert.IsTrue(models.Models.Count > 0);
+        }
+
+        [TestMethod, ExpectedException(typeof(ServiceResponseException))]
+        public void GetModels_Catch_Exception()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            SpeechModelSet response = new SpeechModelSet()
+            {
+                Models = new List<SpeechModel>()
+                {
+                    new SpeechModel()
+                    {
+                        Description = "TEST",
+                        Language = "pt-br",
+                        Name = "UNIT_TEST",
+                        Rate = 1,
+                        Sessions = Guid.NewGuid().ToString(),
+                        Url = "http://"
+                    }
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(x => { throw new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                     Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                     string.Empty); });
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var models = service.GetModels();
         }
 
         [TestMethod]
@@ -126,6 +168,370 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.UnitTest
             SpeechToTextService service = new SpeechToTextService(client);
 
             var models = service.GetModel(string.Empty);
+        }
+
+        [TestMethod, ExpectedException(typeof(ServiceResponseException))]
+        public void GetModel_Catch_Exception()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            SpeechModelSet response = new SpeechModelSet()
+            {
+                Models = new List<SpeechModel>()
+                {
+                    new SpeechModel()
+                    {
+                        Description = "TEST",
+                        Language = "pt-br",
+                        Name = "UNIT_TEST",
+                        Rate = 1,
+                        Sessions = Guid.NewGuid().ToString(),
+                        Url = "http://"
+                    }
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                 .Returns(x => {
+                     throw new ServiceResponseException(Substitute.For<IResponse>(),
+                                                         Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                         string.Empty);
+                 });
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var models = service.GetModels();
+        }
+
+        [TestMethod]
+        public void CreateSession_Success()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            Session response = new Session()
+            {
+                SessionId = "session_id"
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithArgument(Arg.Any<string>(), Arg.Any<object>())
+                   .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<Session>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var models = service.CreateSession("model_name");
+
+            Assert.IsNotNull(models);
+            client.Received().PostAsync(Arg.Any<string>());
+            Assert.IsFalse(string.IsNullOrEmpty(models.SessionId));
+        }
+
+        [TestMethod]
+        public void CreateSession_Default_Name_Success()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            Session response = new Session()
+            {
+                SessionId = "session_id"
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithArgument(Arg.Any<string>(), Arg.Any<object>())
+                   .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<Session>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var models = service.CreateSession(string.Empty);
+
+            Assert.IsNotNull(models);
+            client.Received().PostAsync(Arg.Any<string>());
+            Assert.IsFalse(string.IsNullOrEmpty(models.SessionId));
+        }
+
+        [TestMethod]
+        public void CreateSession_Catch_Exception()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            Session response = new Session()
+            {
+                SessionId = "session_id"
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(x => {
+                      throw new ServiceResponseException(Substitute.For<IResponse>(),
+                                                          Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                          string.Empty);
+                  });
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var models = service.CreateSession(string.Empty);
+        }
+
+        [TestMethod]
+        public void GetSessionStatus_Success()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<RecognizeStatus>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var sessionStatus = service.GetSessionStatus(new Session() { SessionId = "session_id" });
+
+            Assert.IsNotNull(sessionStatus);
+            client.Received().GetAsync(Arg.Any<string>());
+            Assert.AreEqual(sessionStatus.Session.State, "initialized");
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void GetSessionStatus_SessionId_Empty()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<RecognizeStatus>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var sessionStatus = service.GetSessionStatus(string.Empty);
+        }
+
+        [TestMethod, ExpectedException(typeof(ServiceResponseException))]
+        public void GetSessionStatus_Catch_Exception()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(x => {
+                       throw new ServiceResponseException(Substitute.For<IResponse>(),
+                                                           Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                           string.Empty);
+                   });
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            var sessionStatus = service.GetSessionStatus("session_id");
+        }
+
+        [TestMethod]
+        public void DeleteSession_Success()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<RecognizeStatus>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            service.DeleteSession(new Session() { SessionId = "session_id" });
+
+            client.Received().DeleteAsync(Arg.Any<string>());
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void DeleteSession_SessionId_Empty()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                  .Returns(request);
+
+            request.WithHeader(Arg.Any<string>(), Arg.Any<string>())
+                   .Returns(request);
+
+            request.As<RecognizeStatus>()
+                   .Returns(Task.FromResult(response));
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            service.DeleteSession(string.Empty);
+        }
+
+        [TestMethod, ExpectedException(typeof(ServiceResponseException))]
+        public void DeleteSession_Catch_Exception()
+        {
+            #region Mock IClient
+
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                  .Returns(client);
+
+            RecognizeStatus response = new RecognizeStatus()
+            {
+                Session = new SessionStatus()
+                {
+                    State = "initialized"
+                }
+            };
+
+            IRequest request = Substitute.For<IRequest>();
+            client.DeleteAsync(Arg.Any<string>())
+                   .Returns(x => {
+                       throw new ServiceResponseException(Substitute.For<IResponse>(),
+                                                           Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                           string.Empty);
+                   });
+
+            #endregion
+
+            SpeechToTextService service = new SpeechToTextService(client);
+
+            service.DeleteSession("session_id");
         }
     }
 }

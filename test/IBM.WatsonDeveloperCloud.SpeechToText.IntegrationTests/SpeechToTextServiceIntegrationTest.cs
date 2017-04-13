13fc54c749d8780389a20f948758fbd3c9c524f0
Ajiemar Santiago <ajiemar@gmail.com>
2017-04-13 14:11:59 -0500

    remove intent ft from Conversation.Message integration test, add language model to stt recognize integration test

--- test/IBM.WatsonDeveloperCloud.SpeechToText.IntegrationTests/SpeechToTextServiceIntegrationTest.cs
+++ test/IBM.WatsonDeveloperCloud.SpeechToText.IntegrationTests/SpeechToTextServiceIntegrationTest.cs
@@ -133,7 +133,7 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.IntegrationTests
                 File.OpenRead(@"Assets\test-audio.wav");
 
             var results =
-                service.Recognize(audio.GetMediaTypeFromFile(), audio);
+                service.Recognize(audio.GetMediaTypeFromFile(), audio, "", "en-US_BroadbandModel");
 
             Assert.IsNotNull(results);
             Assert.IsNotNull(results.Results);

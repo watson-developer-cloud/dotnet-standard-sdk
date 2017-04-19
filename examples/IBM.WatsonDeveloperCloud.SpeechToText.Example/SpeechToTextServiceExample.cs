513589fddecd4d874935ebcd867fb3234c39d122
Ajiemar Santiago <ajiemar@gmail.com>
2017-04-19 10:54:30 -0500

    started speech to text examples

--- examples/IBM.WatsonDeveloperCloud.SpeechToText.Example/SpeechToTextServiceExample.cs
+++ examples/IBM.WatsonDeveloperCloud.SpeechToText.Example/SpeechToTextServiceExample.cs
@@ -19,7 +19,10 @@ using IBM.WatsonDeveloperCloud.SpeechToText.v1;
 using IBM.WatsonDeveloperCloud.SpeechToText.v1.Model;
 using IBM.WatsonDeveloperCloud.SpeechToText.v1.Util;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
 {
@@ -28,7 +31,12 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
         private SpeechToTextService _speechToText = new SpeechToTextService();
         private string _path = "test-audio.wav";
         private string _modelToGet = "en-US_BroadbandModel";
+        private string _createdCustomizationID;
         private string _sessionID;
+        private string _wordName = "social";
+        private string _corpusName = "stt_integration";
+        string modelName = "en-US_BroadbandModel";
+        AutoResetEvent autoEvent = new AutoResetEvent(false);
 
         public SpeechToTextServiceExample(string username, string password)
         {
@@ -36,10 +44,43 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
 
             GetModels();
             GetModel(_modelToGet);
-            Recognize();
+            RecognizeBody();
+            RecognizeFormData();
+
             CreateSession();
             GetSessionStatus();
+            RecognizeSessionBody();
+            RecognizeSessionFormData();
             DeleteSession();
+
+            CreateCustomModel();
+            ListCustomModels();
+            ListCustomModel();
+
+            AddCustomCorpus();
+            TrainCustomModel();
+            autoEvent.WaitOne();
+            ListCustomCorpora();
+            GetCustomCorpus();
+            DeleteCustomCorpus();
+            TrainCustomModel();
+            autoEvent.WaitOne();
+
+            AddCustomWords();
+            TrainCustomModel();
+            autoEvent.WaitOne();
+            AddCustomWords();
+            TrainCustomModel();
+            autoEvent.WaitOne();
+            ListCustomWords();
+            ListCustomWord(_wordName);
+            DeleteCustomWord(_wordName);
+            TrainCustomModel();
+            autoEvent.WaitOne();
+
+            ResetCustomModel();
+            UpgradeCustomModel();
+            DeleteCustomModel();
         }
 
         #region Get Models
@@ -105,8 +146,8 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
         }
         #endregion
 
-        #region Recognize
-        private void Recognize()
+        #region Recognize Body
+        private void RecognizeBody()
         {
             using (FileStream fs = File.OpenRead(_path))
             {
@@ -129,6 +170,20 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
                     }
                 }
             }
+        }
+        #endregion
+
+        #region Recognize Form Data
+        private void RecognizeFormData()
+        {
+            FileStream audio = File.OpenRead(@"Assets\test-audio.wav");
+
+            var results = _speechToText.Recognize(audio.GetMediaTypeFromFile(),
+                                  new Metadata()
+                                  {
+                                      PartContentType = audio.GetMediaTypeFromFile()
+                                  },
+                                  audio);
         }
         #endregion
 
@@ -174,12 +229,221 @@ namespace IBM.WatsonDeveloperCloud.SpeechToText.Example
         }
         #endregion
 
+        #region Recognize Session Body
+        private void RecognizeSessionBody()
+        {
+            FileStream audio = File.OpenRead(@"Assets\test-audio.wav");
+
+            var results = _speechToText.RecognizeWithSession(_sessionID, audio.GetMediaTypeFromFile(), audio);
+        }
+        #endregion
+
+        #region Recognize Session Form Data
+        private void RecognizeSessionFormData()
+        {
+            FileStream audio = File.OpenRead(@"Assets\test-audio.wav");
+
+            var results = _speechToText.RecognizeWithSession(_sessionID,
+                                             audio.GetMediaTypeFromFile(),
+                                             new Metadata()
+                                             {
+                                                 PartContentType = audio.GetMediaTypeFromFile()
+                                             },
+                                             audio);
+        }
+        #endregion
+
         #region Delete Session
         private void DeleteSession()
         {
             Console.WriteLine(string.Format("Calling DeleteSession({0})...", _sessionID));
             _speechToText.DeleteSession(_sessionID);
             Console.WriteLine(string.Format("Session {0} deleted.", _sessionID));
+        }
+        #endregion
+
+        #region Create Custom Model
+        private void CreateCustomModel()
+        {
+            var result = _speechToText.CreateCustomModel("STT Custom Model", modelName, "A custom speech to text model created from the example.");
+            _createdCustomizationID = result.CustomizationId;
+        }
+        #endregion
+
+        #region List Custom Models
+        private void ListCustomModels()
+        {
+            var customizations = _speechToText.ListCustomModels();
+        }
+        #endregion
+
+        #region List Custom Model
+        private void ListCustomModel()
+        {
+            var result = _speechToText.ListCustomModel(_createdCustomizationID);
+        }
+        #endregion
+
+        #region Add Custom Corpus
+        private void AddCustomCorpus()
+        {
+            var body =
+                File.OpenRead(@"Assets\test-stt-corpus.txt");
+
+            object result = _speechToText.AddCorpus(_createdCustomizationID,
+                              _corpusName,
+                              false,
+                              body);
+        }
+        #endregion
+
+        #region List Custom Corpora
+        private void ListCustomCorpora()
+        {
+            var result = _speechToText.ListCorpora(_createdCustomizationID);
+        }
+        #endregion
+
+        #region Get Custom Corpus
+        private void GetCustomCorpus()
+        {
+            var result = _speechToText.GetCorpus(_createdCustomizationID, _corpusName);
+        }
+        #endregion
+
+        #region Delete Custom Corpus
+        private void DeleteCustomCorpus()
+        {
+            var result = _speechToText.DeleteCorpus(_createdCustomizationID, _corpusName);
+        }
+        #endregion
+
+        #region Add Custom Words
+        private void AddCustomWords()
+        {
+            object result = _speechToText.AddCustomWords(_createdCustomizationID,
+                                  new Words()
+                                  {
+                                      WordsProperty = new List<Word>()
+                                      {
+                                          new Word()
+                                          {
+                                             DisplayAs = "Watson",
+                                             SoundsLike = new List<string>()
+                                             {
+                                                 "wat son"
+                                             },
+                                             WordProperty = "watson"
+                                          },
+                                          new Word()
+                                          {
+                                             DisplayAs = "C#",
+                                             SoundsLike = new List<string>()
+                                             {
+                                                 "si sharp"
+                                             },
+                                             WordProperty = "csharp"
+                                          },
+                                           new Word()
+                                          {
+                                             DisplayAs = "SDK",
+                                             SoundsLike = new List<string>()
+                                             {
+                                                 "S.D.K."
+                                             },
+                                             WordProperty = "sdk"
+                                          }
+                                      }
+                                  });
+        }
+        #endregion
+
+        #region Add Custom Word
+        private void AddCustomWord()
+        {
+            object result = _speechToText.AddCustomWord(_createdCustomizationID,
+                                  _wordName,
+                                  new WordDefinition()
+                                  {
+                                      DisplayAs = "Social",
+                                      SoundsLike = new List<string>()
+                                             {
+                                                 "so cial"
+                                             }
+                                  });
+        }
+        #endregion
+
+        #region List Custom Words
+        private void ListCustomWords()
+        {
+            var words = _speechToText.ListCustomWords(_createdCustomizationID, null, null);
+        }
+        #endregion
+
+        #region List Custom Word
+        private void ListCustomWord(string wordToList)
+        {
+            var words = _speechToText.ListCustomWord(_createdCustomizationID, _wordName);
+        }
+        #endregion
+
+        #region Delete Custom Word
+        private void DeleteCustomWord(string wordToDelete)
+        {
+            object result = _speechToText.DeleteCustomWord(_createdCustomizationID, _wordName);
+        }
+        #endregion
+
+        #region Reset Custom Model
+        private void ResetCustomModel()
+        {
+            var result = _speechToText.ResetCustomModel(_createdCustomizationID);
+        }
+        #endregion
+
+        #region Upgrade Custom Model
+        private void UpgradeCustomModel()
+        {
+            var result = _speechToText.UpgradeCustomModel(_createdCustomizationID);
+        }
+        #endregion
+
+        #region Delete Custom Model
+        private void DeleteCustomModel()
+        {
+            var result = _speechToText.DeleteCustomModel(_createdCustomizationID);
+        }
+        #endregion
+
+        #region Train Custom Model
+        private void TrainCustomModel()
+        {
+            var result = _speechToText.TrainCustomModel(_createdCustomizationID);
+            IsTrainingComplete();
+        }
+        #endregion
+
+        #region Is Training Complete
+        private bool IsTrainingComplete()
+        {
+            var result = _speechToText.ListCustomModel(_createdCustomizationID);
+
+            string status = result.Status.ToLower();
+            Console.WriteLine(string.Format("Classifier status is {0}", status));
+
+            if (status == "ready" || status == "available")
+                autoEvent.Set();
+            else
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    IsTrainingComplete();
+                });
+            }
+
+            return result.Status.ToLower() == "ready";
         }
         #endregion
     }

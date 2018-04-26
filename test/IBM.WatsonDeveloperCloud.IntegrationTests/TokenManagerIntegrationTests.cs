23de92a20718d72c2a16f8a5c477545ec2592d9c
Ajiemar <ajiemar@gmail.com>
2018-04-26 18:21:49 -0500

    do not test tokens

--- test/IBM.WatsonDeveloperCloud.IntegrationTests/TokenManagerIntegrationTests.cs
+++ test/IBM.WatsonDeveloperCloud.IntegrationTests/TokenManagerIntegrationTests.cs
@@ -23,7 +23,7 @@ using System;
 
 namespace IBM.WatsonDeveloperCloud.IntegrationTests
 {
-    [TestClass]
+    //[TestClass]
     public class TokenManagerIntegrationTests
     {
         private TokenManager _iamAssistantTokenManager;

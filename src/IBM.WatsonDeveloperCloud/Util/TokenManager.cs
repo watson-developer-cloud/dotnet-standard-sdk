45152ef993aeba3c875e5154613b2a9f2e9af73c
Ajiemar <ajiemar@gmail.com>
2018-05-01 11:43:08 -0500

    move credentials data models to Credentials.cs, revise Assistant tests to comsume local credentials

--- src/IBM.WatsonDeveloperCloud/Util/TokenManager.cs
+++ src/IBM.WatsonDeveloperCloud/Util/TokenManager.cs
@@ -230,28 +230,4 @@ namespace IBM.WatsonDeveloperCloud.Util
             _tokenInfo = tokenResponse;
         }
     }
-
-    public class TokenOptions
-    {
-        [JsonProperty("iamApiKey", NullValueHandling = NullValueHandling.Ignore)]
-        public string IamApiKey { get; set; }
-        [JsonProperty("iamAcessToken", NullValueHandling = NullValueHandling.Ignore)]
-        public string IamAccessToken { get; set; }
-        [JsonProperty("iamUrl", NullValueHandling = NullValueHandling.Ignore)]
-        public string IamUrl { get; set; }
-    }
-
-    public class IamTokenData
-    {
-        [JsonProperty("access_token", NullValueHandling = NullValueHandling.Ignore)]
-        public string AccessToken { get; set; }
-        [JsonProperty("refresh_token", NullValueHandling = NullValueHandling.Ignore)]
-        public string RefreshToken { get; set; }
-        [JsonProperty("token_type", NullValueHandling = NullValueHandling.Ignore)]
-        public string TokenType { get; set; }
-        [JsonProperty("expires_in", NullValueHandling = NullValueHandling.Ignore)]
-        public long? ExpiresIn { get; set; }
-        [JsonProperty("expiration", NullValueHandling = NullValueHandling.Ignore)]
-        public long? Expiration { get; set; }
-    }
 }

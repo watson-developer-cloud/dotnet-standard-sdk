42536f78a3582ce54a7d5229ac83d282ec5df82c
Ajiemar Santiago <ajiemar@gmail.com>
2018-07-31 13:52:01 -0500

    chore(tests): do not check for github token env variable so we can run unit tests

--- coverage.ps1
+++ coverage.ps1
@@ -21,21 +21,11 @@ Copy-Item .\test\IBM.WatsonDeveloperCloud.Discovery.v1.IntegrationTests\Discover
 
 $openCover = '.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe'
 
-ForEach($projectFolder in (Get-ChildItem -Path .\src -Directory))
-{
-    $projectJson = $projectFolder.FullName + '\project.json'
-    echo 'building ' $projectFolder.Name
-    dotnet build $projectJson
-}
-
 ForEach ($folder in (Get-ChildItem -Path .\test -Directory)) 
-{ 
-    if(!$folder.Name.Contains("TextToSpeech"))
-    {  
-        $targetArgs = '-targetargs: test \"' + $folder.FullName + '\" -c Debug -f netcoreapp1.0'
-        $filter = '-filter:+[IBM.WatsonDeveloperCloud*]*-[*Tests*]*-[*Example*]*'
-        & $openCover '-target:C:\Program Files\dotnet\dotnet.exe' $targetArgs '-register:user' $filter '-oldStyle' '-mergeoutput' '-hideskipped:File' '-searchdirs:$testdir\bin\Debug\netstandard1.6' '-output:coverage\coverage.xml'
-    }
+{
+    $targetArgs = '-targetargs: test ' + $folder.FullName + ' -c Release -f netcoreapp1.0'
+    $filter = '-filter:+[IBM.WatsonDeveloperCloud*]*-[*Tests*]*-[*Example*]*'
+    & $openCover '-target:C:\\Program Files\\dotnet\\dotnet.exe' $targetArgs '-register:user' $filter '-oldStyle' '-mergeoutput' '-hideskipped:File' '-searchdirs:$testdir\\bin\\release\\netcoreapp1.0' '-output:coverage\\coverage.xml'
 }
 
 $reportGenerator = '.\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe'

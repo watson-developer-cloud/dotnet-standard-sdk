1d240121c9ac7829b95d28747eb930e813302ca8
Ajiemar Santiago <ajiemar@gmail.com>
2018-12-06 13:41:29 -0600

    chore: Cleaned up project with respect to LTv2

--- examples/IBM.WatsonDeveloperCloud.LanguageTranslator.v3.Example/Properties/AssemblyInfo.cs
+++ examples/IBM.WatsonDeveloperCloud.LanguageTranslator.v3.Example/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IBM")]
-[assembly: AssemblyProduct("IBM.WatsonDeveloperCloud.LanguageTranslator.v2.Example")]
+[assembly: AssemblyProduct("IBM.WatsonDeveloperCloud.LanguageTranslator.v3.Example")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible

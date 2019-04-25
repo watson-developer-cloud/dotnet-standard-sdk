0dd2d9bb343fcd8f58f2d3a29090276aaa805fe4
Ajiemar Santiago <ajiemar@gmail.com>
2019-04-25 12:41:58 -0500

    chore: Remove unnecessary conveters

--- test/IBM.Watson.IntegrationTests/AdditionalPropertiesTest.cs
+++ test/IBM.Watson.IntegrationTests/AdditionalPropertiesTest.cs
@@ -18,9 +18,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace IBM.Watson.IntegrationTests
 {
@@ -46,10 +44,10 @@ namespace IBM.Watson.IntegrationTests
             foo2.Ack = true;
             x.Add("myFoo2", foo2);
 
-            var json = JsonConvert.SerializeObject(x, new XConverter(typeof(X)));
+            var json = JsonConvert.SerializeObject(x);
 
             Assert.IsTrue(json == "{\"prop1\":\"string\",\"prop2\":42,\"myFoo\":{\"bar\":\"bar\",\"fum\":\"fum\",\"ack\":false},\"myFoo2\":{\"bar\":\"bar2\",\"fum\":\"fum2\",\"ack\":true}}");
-            var x2 = JsonConvert.DeserializeObject<X>(json, new XConverter(typeof(X)));
+            var x2 = JsonConvert.DeserializeObject<X>(json);
 
             Assert.IsTrue(x2.Prop1 == "string");
             Assert.IsTrue(x2.Prop2 == 42);
@@ -77,10 +75,10 @@ namespace IBM.Watson.IntegrationTests
             y.Add("baz", "baz");
             y.Add("qux", 1.23f);
 
-            var json = JsonConvert.SerializeObject(y, new YConverter(typeof(Y)));
+            var json = JsonConvert.SerializeObject(y);
             Assert.IsTrue(json == "{\"prop1\":\"string\",\"prop2\":42,\"myFoo\":{\"bar\":\"bar\",\"fum\":\"fum\",\"ack\":false},\"baz\":\"baz\",\"qux\":1.23}");
 
-            var y2 = JsonConvert.DeserializeObject<Y>(json, new YConverter(typeof(Y)));
+            var y2 = JsonConvert.DeserializeObject<Y>(json);
 
             Assert.IsTrue(y2.Prop1 == "string");
 
@@ -121,77 +119,7 @@ namespace IBM.Watson.IntegrationTests
         public bool Ack { get; set; }
     }
 
-    #region XConverter
-    public class XConverter : JsonConverter
-    {
-        private readonly Type[] _types;
-
-        public XConverter(params Type[] types)
-        {
-            _types = types;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return _types.Any(t => t == objectType);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            X x = new X();
-
-            JObject item = JObject.Load(reader);
-
-            if (!string.IsNullOrEmpty(item["prop1"].ToString()))
-            {
-                x.Prop1 = item["prop1"].ToString();
-            }
-            item.Remove("prop1");
-
-            if (item["prop2"] != null)
-            {
-                long.TryParse(item["prop2"].ToString(), out long value);
-                x.Prop2 = value;
-            }
-            item.Remove("prop2");
-
-            IList<string> propertyNames = item.Properties().Select(p => p.Name).ToList();
-            foreach (string prop in propertyNames)
-            {
-                var value = JsonConvert.DeserializeObject<Foo>(item[prop].ToString());
-                x.Add(prop, value);
-            }
-
-            return x;
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            JToken t = JToken.FromObject(value);
-
-            if (t.Type != JTokenType.Object)
-            {
-                t.WriteTo(writer);
-            }
-            else
-            {
-                JObject o = (JObject)t;
-                JToken jt = o.GetValue("AdditionalProperties");
-                o.Remove("AdditionalProperties");
-                JObject ojt = (JObject)jt;
-                IList<string> propertyNames = ojt.Properties().Select(p => p.Name).ToList();
-                foreach (string prop in propertyNames)
-                {
-                    o[prop] = ojt[prop];
-                }
-
-                o.WriteTo(writer);
-            }
-        }
-    }
-    #endregion
-
-    public class Y : DynamicModel<object>
+    public class Y : DynamicModel
     {
         [JsonProperty("prop1", NullValueHandling = NullValueHandling.Ignore)]
         public string Prop1 { get; set; }
@@ -199,95 +127,14 @@ namespace IBM.Watson.IntegrationTests
         public long Prop2 { get; set; }
     }
 
-    #region YConverter
-    public class YConverter : JsonConverter
-    {
-        private readonly Type[] _types;
-
-        public YConverter(params Type[] types)
-        {
-            _types = types;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return _types.Any(t => t == objectType);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            Y y = new Y();
-
-            JObject item = JObject.Load(reader);
-
-            if (!string.IsNullOrEmpty(item["prop1"].ToString()))
-            {
-                y.Prop1 = item["prop1"].ToString();
-            }
-            item.Remove("prop1");
-
-            if (item["prop2"] != null)
-            {
-                long.TryParse(item["prop2"].ToString(), out long value);
-                y.Prop2 = value;
-            }
-            item.Remove("prop2");
-
-            IList<string> propertyNames = item.Properties().Select(p => p.Name).ToList();
-            foreach (string prop in propertyNames)
-            {
-                switch (item[prop].Type)
-                {
-                    case JTokenType.String:
-                        y.Add(prop, item[prop].ToString());
-                        break;
-                    case JTokenType.Float:
-                        float.TryParse(item[prop].ToString(), out float value);
-                        y.Add(prop, value);
-                        break;
-                    default:
-                        var o = JsonConvert.DeserializeObject<JObject>(item[prop].ToString());
-                        y.Add(prop, o);
-                        break;
-                }
-            }
-
-            return y;
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            JToken t = JToken.FromObject(value);
-
-            if (t.Type != JTokenType.Object)
-            {
-                t.WriteTo(writer);
-            }
-            else
-            {
-                JObject o = (JObject)t;
-                JToken jt = o.GetValue("AdditionalProperties");
-                o.Remove("AdditionalProperties");
-                JObject ojt = (JObject)jt;
-                IList<string> propertyNames = ojt.Properties().Select(p => p.Name).ToList();
-                foreach (string prop in propertyNames)
-                {
-                    o[prop] = ojt[prop];
-                }
-
-                o.WriteTo(writer);
-            }
-        }
-    }
-    #endregion
-
     public class DynamicModel<T>
     {
-        public Dictionary<string, T> AdditionalProperties { get; } = new Dictionary<string, T>();
+        [JsonExtensionData]
+        public Dictionary<string, JToken> AdditionalProperties { get; } = new Dictionary<string, JToken>();
 
         public void Add(string key, T value)
         {
-            AdditionalProperties.Add(key, value);
+            AdditionalProperties.Add(key, JToken.FromObject(value));
         }
 
         public void Remove(string key)
@@ -297,7 +144,29 @@ namespace IBM.Watson.IntegrationTests
 
         public T Get(string key)
         {
-            AdditionalProperties.TryGetValue(key, out T value);
+            AdditionalProperties.TryGetValue(key, out JToken value);
+            return value.ToObject<T>();
+        }
+    }
+
+    public class DynamicModel
+    {
+        [JsonExtensionData]
+        public Dictionary<string, JToken> AdditionalProperties { get; } = new Dictionary<string, JToken>();
+
+        public void Add(string key, object value)
+        {
+            AdditionalProperties.Add(key, JToken.FromObject(value));
+        }
+
+        public void Remove(string key)
+        {
+            AdditionalProperties.Remove(key);
+        }
+
+        public JToken Get(string key)
+        {
+            AdditionalProperties.TryGetValue(key, out JToken value);
             return value;
         }
     }

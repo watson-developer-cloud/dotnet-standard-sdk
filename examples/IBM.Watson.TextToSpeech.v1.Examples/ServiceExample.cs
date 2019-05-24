0f7ec0a8ba138c2fd83bb70e0fc6868146e49daf
Ajiemar Santiago <ajiemar@gmail.com>
2019-05-24 11:10:18 -0500

    chore(Text to Speech V1): Add examples

--- examples/IBM.Watson.TextToSpeech.v1.Examples/ServiceExample.cs
+++ examples/IBM.Watson.TextToSpeech.v1.Examples/ServiceExample.cs
@@ -15,7 +15,11 @@
 *
 */
 
+using IBM.Cloud.SDK.Core.Util;
+using IBM.Watson.TextToSpeech.v1.Model;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace IBM.Watson.TextToSpeech.v1.Examples
 {
@@ -23,10 +27,336 @@ namespace IBM.Watson.TextToSpeech.v1.Examples
     {
         string apikey = "{apikey}";
         string url = "{url}";
+        string customizationId;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServiceExample example = new ServiceExample();
+
+            Console.WriteLine("Examples complete. Press any key to close the application.");
+            Console.ReadKey();
         }
+
+        #region Voices
+        public void ListVoices()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.ListVoices();
+
+            Console.WriteLine(result.Result);
+        }
+
+        public void GetVoice()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.GetVoice("en-US_AllisonVoice");
+
+            Console.WriteLine(result.Result);
+        }
+        #endregion
+
+        #region Synthesis
+        public void Synthesize()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.Synthesize(
+                text: "Hello, welcome to the Watson dotnet SDK!",
+                accept: "audio/wav",
+                voice: "en-US_AllisonVoice"
+                );
+
+            //  Save file
+            using (FileStream fs = File.Create("synthesize.wav"))
+            {
+                result.Result.WriteTo(fs);
+                fs.Close();
+                result.Result.Close();
+            }
+
+            Console.WriteLine(result.Result);
+        }
+        #endregion
+
+        #region Pronunciation
+        public void GetPronunciation()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.GetPronunciation(
+                text: "IBM",
+                voice: "en-US_AllisonVoice",
+                format: "ipa"
+                );
+
+            Console.WriteLine(result.Result);
+        }
+        #endregion
+
+        #region Custom Models
+        public void ListVoiceModels()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.ListVoiceModels();
+
+            Console.WriteLine(result.Result);
+        }
+
+        public void CreateVoiceModels()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.CreateVoiceModel(
+                name: "dotnet-sdk-voice-model",
+                language: "en-US",
+                description: "Custom voice model for .NET SDK examples.");
+            customizationId = result.Result.CustomizationId;
+
+            Console.WriteLine(result.Result);
+        }
+
+        public void GetVoiceModel()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.GetVoiceModel(
+                customizationId: customizationId
+                );
+
+            Console.WriteLine(result.Result);
+        }
+
+        public void UpdateVoiceModel()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var words = new List<Word>()
+            {
+                new Word()
+                {
+                    _Word = "hello",
+                    Translation = "hullo"
+                },
+                new Word()
+                {
+                    _Word = "goodbye",
+                    Translation = "gbye"
+                },
+                new Word()
+                {
+                    _Word = "hi",
+                    Translation = "ohioooo"
+                }
+            };
+
+            var result = service.UpdateVoiceModel(
+                customizationId: customizationId,
+                name: "dotnet-sdk-voice-model-updated",
+                description: "Custom voice model for .NET SDK integration tests. Updated.",
+                words: words
+                );
+
+            Console.WriteLine(result.Result);
+        }
+
+        public void DeleteVoiceModel()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.DeleteVoiceModel(
+                customizationId: customizationId
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+        #endregion
+
+        #region Custom Words
+        private void AddWords()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var words = new List<Word>()
+            {
+                new Word()
+                {
+                    _Word = "hello",
+                    Translation = "hullo"
+                },
+                new Word()
+                {
+                    _Word = "goodbye",
+                    Translation = "gbye"
+                },
+                new Word()
+                {
+                    _Word = "hi",
+                    Translation = "ohioooo"
+                }
+            };
+
+            var result = service.AddWords(
+                customizationId: customizationId,
+                words: words
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+
+        private void ListWords()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.ListWords(
+                customizationId: customizationId
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+
+        private void AddWord()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.AddWord(
+                customizationId: customizationId,
+                word: "IBM",
+                translation: "eye bee m",
+                partOfSpeech: "noun"
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+
+        private void GetWord()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.GetWord(
+                customizationId: customizationId,
+                word: "hello"
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+
+        private void DeleteWord()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.DeleteWord(
+                customizationId: customizationId,
+                word: "hello"
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+        #endregion
+
+        #region User Data
+        public void DeleteUserData()
+        {
+            TokenOptions tokenOptions = new TokenOptions()
+            {
+                IamApiKey = apikey,
+                ServiceUrl = url
+            };
+
+            TextToSpeechService service = new TextToSpeechService(tokenOptions);
+
+            var result = service.DeleteUserData(
+                customerId: "customerId"
+                );
+
+            Console.WriteLine(result.StatusCode);
+        }
+        #endregion
     }
 }

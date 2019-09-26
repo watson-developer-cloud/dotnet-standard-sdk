ea4af3bfbfbbe17b8fd63ec1f9845d8ebdd81208
Ajiemar Santiago <ajiemar@gmail.com>
2019-09-26 17:30:55 -0500

    chore(Visual Recognition V4): Regenerate the service based on recent updates

--- src/IBM.Watson.VisualRecognition.v4/Model/Collection.cs
+++ src/IBM.Watson.VisualRecognition.v4/Model/Collection.cs
@@ -59,7 +59,7 @@ namespace IBM.Watson.VisualRecognition.v4.Model
         /// Training status information for the collection.
         /// </summary>
         [JsonProperty("training_status", NullValueHandling = NullValueHandling.Ignore)]
-        public BaseCollectionTrainingStatus TrainingStatus { get; set; }
+        public virtual TrainingStatus TrainingStatus { get; private set; }
     }
 
 }

270ccbb2c91c1b5e87ae8875e094e4780357b67b
Ajiemar Santiago <ajiemar@gmail.com>
2019-10-10 08:36:08 -0500

    test(Unit tests): Handwrite unit tests in preperation for generation

---
+++ singleUnit.ps1
@@ -0,0 +1,26 @@
+﻿if(Test-Path -Path coverage)
+{
+  Remove-Item .\coverage -recurse
+}
+
+dotnet restore
+
+if(-Not (Test-Path -Path '\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe'))
+{
+    nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
+}
+$openCover = '.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe'
+
+if(-Not (Test-Path -Path '\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe'))
+{
+    nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 ReportGenerator
+}
+$reportGenerator = '.\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe'
+
+New-Item -path . -name coverage -itemtype directory
+
+$targetArgs = '-targetargs: test test\VisualRecognition.v4.UnitTests -c Release -f netcoreapp2.0'
+$filter = '-filter:+[IBM.Watson*]*-[*Tests*]*-[*Example*]*'
+& $openCover '-target:C:\\Program Files\\dotnet\\dotnet.exe' $targetArgs '-register:user' $filter '-oldStyle' '-mergeoutput' '-hideskipped:File' '-searchdirs:$testdir\\bin\\release\\netcoreapp2.0' '-output:coverage\\coverage.xml'
+
+& $reportGenerator -reports:coverage\coverage.xml -targetdir:coverage -verbosity:Error
--- test/VisualRecognition.v4.UnitTests/VisualRecognitionUnitTests.cs
+++ test/VisualRecognition.v4.UnitTests/VisualRecognitionUnitTests.cs
@@ -24,6 +24,10 @@ using IBM.Watson.VisualRecognition.v4.Model;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using IBM.Cloud.SDK.Core.Http.Exceptions;
+using System.Net;
 
 namespace IBM.Watson.VisualRecognition.v4.UnitTests
 {
@@ -39,12 +43,18 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
         }
 
         [TestMethod]
-        public void Constructor_HttpClient()
+        public void ConstructorHttpClient()
         {
             VisualRecognitionService service =
-                new VisualRecognitionService(CreateClient());
+                new VisualRecognitionService(new IBMHttpClient());
 
             Assert.IsNotNull(service);
+        }
+
+        [TestMethod]
+        public void ConstructorExternalConfig()
+        {
+            VisualRecognitionService service = Substitute.For<VisualRecognitionService>("verisionDate");
         }
 
         [TestMethod]
@@ -57,7 +67,7 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
         }
 
         [TestMethod]
-        public void Constructor_Authenticator()
+        public void ConstructorAuthenticator()
         {
             VisualRecognitionService service = new VisualRecognitionService("versionDate", new NoAuthAuthenticator());
 
@@ -65,9 +75,22 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
         }
 
         [TestMethod, ExpectedException(typeof(ArgumentNullException))]
-        public void Constructor_NoVersion()
+        public void ConstructorNoVersion()
         {
             VisualRecognitionService service = new VisualRecognitionService(null, new NoAuthAuthenticator());
+        }
+
+        [TestMethod]
+        public void ConstructorNoUrl()
+        {
+            var url = Environment.GetEnvironmentVariable("VISUAL_RECOGNITION_SERVICE_URL");
+            Environment.SetEnvironmentVariable("VISUAL_RECOGNITION_SERVICE_URL", null);
+
+            VisualRecognitionService service = Substitute.For<VisualRecognitionService>("versionDate");
+
+            Assert.IsTrue(service.ServiceUrl == "https://gateway.watsonplatform.net/visual-recognition/api");
+
+            Environment.SetEnvironmentVariable("VISUAL_RECOGNITION_SERVICE_URL", url);
         }
         #endregion
 
@@ -128,7 +151,7 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
                 }
             };
 
-            var response =new DetailedResponse<AnalyzeResponse>();
+            var response = new DetailedResponse<AnalyzeResponse>();
             response.Result = analyzeResponse;
             #endregion
 
@@ -164,18 +187,329 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
         #endregion
 
         #region CreateCollection
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void CreateCollectionNoVersionDate()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+            service.VersionDate = null;
+
+            var result = service.CreateCollection(
+                name: "collectionName",
+                description: "collectionDescription"
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void CreateCollectionCatchException()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                 .Returns(x =>
+                 {
+                     throw new AggregateException(new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                               Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                               string.Empty));
+                 });
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+
+            var result = service.CreateCollection(
+                name: "collectionName",
+                description: "collectionDescription"
+                );
+        }
+
+        [TestMethod]
+        public void CreateCollection()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                .Returns(request);
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+            var result = service.CreateCollection(
+                name: "collectionName",
+                description: "collectionDescription"
+                );
+
+            JObject bodyObject = new JObject();
+            bodyObject["name"] = "collectionName";
+            bodyObject["description"] = "collectionDescription";
+            var json = JsonConvert.SerializeObject(bodyObject);
+
+            request.Received().WithArgument("version", "versionDate");
+            request.Received().WithBodyContent(Arg.Is<StringContent>(x => x.ReadAsStringAsync().Result.Equals(json)));
+
+        }
         #endregion
 
         #region ListCollections
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ListCollectionsNoVersionDate()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+            service.VersionDate = null;
+
+            var result = service.ListCollections();
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void ListCollectionCatchException()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                 .Returns(x =>
+                 {
+                     throw new AggregateException(new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                               Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                               string.Empty));
+                 });
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+
+            var result = service.ListCollections();
+        }
+
+        [TestMethod]
+        public void ListCollections()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                .Returns(request);
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+            var result = service.ListCollections();
+
+            request.Received().WithArgument("version", "versionDate");
+        }
         #endregion
 
         #region GetCollection
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void GetCollectionsNoVersionDate()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+            service.VersionDate = null;
+
+            var result = service.GetCollection(
+                collectionId: "collectionId"
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void GetCollectionsNoCollectionId()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+
+            var result = service.GetCollection(
+                collectionId: null
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void GetCollectionCatchException()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                 .Returns(x =>
+                 {
+                     throw new AggregateException(new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                               Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                               string.Empty));
+                 });
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+
+            var result = service.GetCollection(
+                collectionId: "collectionId"
+                );
+        }
+
+        [TestMethod]
+        public void GetCollection()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.GetAsync(Arg.Any<string>())
+                .Returns(request);
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+            var result = service.GetCollection(
+                collectionId: "collectionId"
+                );
+
+            request.Received().WithArgument("version", "versionDate");
+            client.Received().GetAsync($"{service.ServiceUrl}/v4/collections/{"collectionId"}");
+        }
         #endregion
 
         #region UpdateCollection
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateCollectionsNoVersionDate()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+            service.VersionDate = null;
+
+            var result = service.UpdateCollection(
+                collectionId: "collectionId"
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateCollectionsNoCollectionId()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+
+            var result = service.UpdateCollection(
+                collectionId: null
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void UpdateCollectionCatchException()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                 .Returns(x =>
+                 {
+                     throw new AggregateException(new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                               Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                               string.Empty));
+                 });
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+
+            var result = service.UpdateCollection(
+                collectionId: "collectionId",
+                name: "collectionName",
+                description: "collectionDescription"
+                );
+        }
+
+        [TestMethod]
+        public void UpdateCollections()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.PostAsync(Arg.Any<string>())
+                .Returns(request);
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+            var result = service.UpdateCollection(
+                collectionId: "collectionId",
+                name: "collectionName",
+                description: "collectionDescription"
+                );
+
+            JObject bodyObject = new JObject();
+            bodyObject["name"] = "collectionName";
+            bodyObject["description"] = "collectionDescription";
+            var json = JsonConvert.SerializeObject(bodyObject);
+
+            request.Received().WithArgument("version", "versionDate");
+            request.Received().WithBodyContent(Arg.Is<StringContent>(x => x.ReadAsStringAsync().Result.Equals(json)));
+            client.Received().PostAsync($"{service.ServiceUrl}/v4/collections/{"collectionId"}");
+        }
         #endregion
 
         #region DeleteCollection
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void DeleteCollectionsNoVersionDate()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+            service.VersionDate = null;
+
+            var result = service.DeleteCollection(
+                collectionId: "collectionId"
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void DeleteCollectionsNoCollectionId()
+        {
+            VisualRecognitionService service = new VisualRecognitionService(
+                versionDate: "versionDate",
+                authenticator: new NoAuthAuthenticator()
+                );
+
+            var result = service.DeleteCollection(
+                collectionId: null
+                );
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void DeleteCollectionCatchException()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.DeleteAsync(Arg.Any<string>())
+                 .Returns(x =>
+                 {
+                     throw new AggregateException(new ServiceResponseException(Substitute.For<IResponse>(),
+                                                                               Substitute.For<HttpResponseMessage>(HttpStatusCode.BadRequest),
+                                                                               string.Empty));
+                 });
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+
+            var result = service.DeleteCollection(
+                collectionId: "collectionId"
+                );
+        }
+
+        [TestMethod]
+        public void DeleteCollection()
+        {
+            IClient client = Substitute.For<IClient>();
+            IRequest request = Substitute.For<IRequest>();
+            client.DeleteAsync(Arg.Any<string>())
+                .Returns(request);
+
+            VisualRecognitionService service = new VisualRecognitionService(client);
+            service.VersionDate = "versionDate";
+            var result = service.DeleteCollection(
+                collectionId: "collectionId"
+                );
+
+            request.Received().WithArgument("version", "versionDate");
+            client.Received().DeleteAsync($"{service.ServiceUrl}/v4/collections/{"collectionId"}");
+        }
         #endregion
 
         #region AddImages
@@ -207,12 +541,12 @@ namespace IBM.Watson.VisualRecognition.v4.UnitTests
         {
             IClient client = Substitute.For<IClient>();
 
-            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+            client.WithAuthentication(Arg.Any<string>())
                 .Returns(client);
 
             return client;
         }
         #endregion
-        
+
     }
 }

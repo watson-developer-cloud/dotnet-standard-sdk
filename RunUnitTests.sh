21a88e38b857bbaa2b7ea1ab721421a6ceb2a277
Taj Santiago <ajiemar@gmail.com>
2019-10-21 11:40:36 -0500

    feat(Code Coverage): update to coverlet

---
+++ RunUnitTests.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+if [ -d "coverage" ]; then
+    rm -rf coverage
+fi
+
+dotnet test test/Assistant.v1.UnitTests /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../../coverage/lcov
+dotnet ~/.nuget/packages/reportgenerator/4.1.5/tools/netcoreapp2.0/ReportGenerator.dll "-reports:coverage/lcov.opencover.xml" "-targetdir:coverage/report"

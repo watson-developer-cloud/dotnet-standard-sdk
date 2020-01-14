f27c9ef831295d57a8f7d994a29abd7247afda93
Ajiemar Santiago <ajiemar@gmail.com>
2020-01-14 17:03:17 -0600

    chore(Common): Update common dependency

--- src/IBM.Watson.Common/Common.cs
+++ src/IBM.Watson.Common/Common.cs
@@ -27,7 +27,7 @@ namespace IBM.Watson
         /// <summary>
         /// The SDK version.
         /// </summary>
-        public const string Version = "watson-apis-dotnet-standard-sdk-4.1.0";
+        public const string Version = "watson-apis-dotnet-standard-sdk-4.2.0";
         private static string os;
         private static string osVersion;
         private static string frameworkDescription;

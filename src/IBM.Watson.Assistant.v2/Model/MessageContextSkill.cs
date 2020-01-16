60b05ce171830306d6e0bf9dcc97268adaa16fa6
Ajiemar Santiago <ajiemar@gmail.com>
2020-01-16 11:01:52 -0600

    chore(Regeneration): Regenerate and run fix copyrights script

--- src/IBM.Watson.Assistant.v2/Model/MessageContextSkill.cs
+++ src/IBM.Watson.Assistant.v2/Model/MessageContextSkill.cs
@@ -30,6 +30,11 @@ namespace IBM.Watson.Assistant.v2.Model
         /// </summary>
         [JsonProperty("user_defined", NullValueHandling = NullValueHandling.Ignore)]
         public Dictionary<string, object> UserDefined { get; set; }
+        /// <summary>
+        /// For internal use only.
+        /// </summary>
+        [JsonProperty("system", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, object> System { get; set; }
     }
 
 }

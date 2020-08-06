54fd8f5d331f29cc7069fd8e78215b873d73f012
Kevin Kowalski <kevinkowalski24@gmail.com>
2020-08-06 10:00:13 -0500

    feat(regeneration): aPI def commit `ad3b2b87287465b4fd4e3c16665a18e40517ce9a` and gen tag `2.3.1`

---
+++ src/IBM.Watson.Discovery.v2/Model/DefaultQueryParamsTableResults.cs
@@ -0,0 +1,45 @@
+/**
+* (C) Copyright IBM Corp. 2018, 2020.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using Newtonsoft.Json;
+
+namespace IBM.Watson.Discovery.v2.Model
+{
+    /// <summary>
+    /// Default project query settings for table results.
+    /// </summary>
+    public class DefaultQueryParamsTableResults
+    {
+        /// <summary>
+        /// When `true`, a table results for the query are retuned by default.
+        /// </summary>
+        [JsonProperty("enabled", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Enabled { get; set; }
+        /// <summary>
+        /// The number of table results to return by default.
+        /// </summary>
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Count { get; set; }
+        /// <summary>
+        /// When `true` the table results are returned as part of the result document and not in a separate object by
+        /// default.
+        /// </summary>
+        [JsonProperty("per_document", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? PerDocument { get; set; }
+    }
+
+}
---
+++ src/IBM.Watson.Discovery.v2/Model/Enrichment.cs
@@ -0,0 +1,54 @@
+/**
+* (C) Copyright IBM Corp. 2018, 2020.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using Newtonsoft.Json;
+
+namespace IBM.Watson.Discovery.v2.Model
+{
+    /// <summary>
+    /// Information about a specific enrichment.
+    /// </summary>
+    public class Enrichment
+    {
+        /// <summary>
+        /// The unique identifier of this enrichment.
+        /// </summary>
+        [JsonProperty("enrichment_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string EnrichmentId { get; set; }
+        /// <summary>
+        /// The human readable name for this enrichment.
+        /// </summary>
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+        /// <summary>
+        /// The description of this enrichment.
+        /// </summary>
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+        /// <summary>
+        /// The type of this enrichment.
+        /// </summary>
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+        /// <summary>
+        /// A object containing options for the current enrichment.
+        /// </summary>
+        [JsonProperty("options", NullValueHandling = NullValueHandling.Ignore)]
+        public EnrichmentOptions Options { get; set; }
+    }
+
+}

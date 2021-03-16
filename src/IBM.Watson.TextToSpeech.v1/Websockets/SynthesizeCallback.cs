585e847b3ab2539ac872ed88452fbeb96774a8f0
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-03-16 15:00:44 -0500

    feat(text-to-speech): add websockets to text-to-speech

---
+++ src/IBM.Watson.TextToSpeech.v1/Websockets/SynthesizeCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace IBM.Watson.TextToSpeech.v1.Websockets
+{
+    public class SynthesizeCallback
+    {
+        public Action OnOpen = () => { };
+        public Action<byte[]> OnMessage = (message) => { };
+        public Action<string> OnContentType = (contentType) => { };
+        public Action<MarkTiming[]> OnMarks = (marks) => { };
+        public Action<WordTiming[]> onTimings = (timings) => { };
+        public Action<Exception> OnError = (ex) => { };
+        public Action OnClose = () => { };
+    }
+}

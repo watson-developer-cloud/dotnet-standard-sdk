72fac90194a3e725d936a82b814640f108ce0df7
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-03-16 14:58:54 -0500

    feat(timings): add word timings & mark timings

---
+++ src/IBM.Watson.TextToSpeech.v1/Websockets/WordTiming.cs
@@ -0,0 +1,21 @@
+﻿namespace IBM.Watson.TextToSpeech.v1.Websockets
+{
+    public class WordTiming
+    {
+        public string word { get; }
+        public double startTime { get; }
+        public double endTime { get; }
+
+        public WordTiming(string word, double startTime, double endTime)
+        {
+            this.word = word;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public override string ToString()
+        {
+            return word + " " + startTime + "-" + endTime;
+        }
+    }
+}

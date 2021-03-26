91975e07fbcff9f5cff458408dd8c73a9b13b1ff
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-03-26 11:13:11 -0500

    feat(speech-to-text): add recognize using websockets

---
+++ src/IBM.Watson.SpeechToText.v1/Websockets/RecognizeCallback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IBM.Watson.SpeechToText.v1.Model;
+
+namespace IBM.Watson.SpeechToText.v1.Websockets
+{
+    public class RecognizeCallback
+    {
+        public Action OnOpen = () => { };
+        public Action<SpeechRecognitionResults> OnTranscription = (speechResults) => { };
+        public Action<Exception> OnError = (ex) => { };
+        public Action OnClose = () => { };
+    }
+}

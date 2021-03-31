e6196a3fa07c2a5abe2959a4828e51fb90a93e2f
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-03-31 09:07:15 -0500

    fix(speech-to-text): change naming convention for websockets

--- src/IBM.Watson.SpeechToText.v1/SpeechToTextServiceExtension.cs
+++ src/IBM.Watson.SpeechToText.v1/SpeechToTextServiceExtension.cs
@@ -9,7 +9,7 @@ namespace IBM.Watson.SpeechToText.v1
 {
     public partial class SpeechToTextService : IBMService, ISpeechToTextService
     {
-        public WebSocketClient RecognizeUsingWebsockets(RecognizeCallback callback, System.IO.MemoryStream audio, string contentType = null, string model = null, string languageCustomizationId = null, string acousticCustomizationId = null, string baseModelVersion = null, double? customizationWeight = null, long? inactivityTimeout = null, bool? interimResults = null, List<string> keywords = null, float? keywordsThreshold = null, long? maxAlternatives = null, float? wordAlternativesThreshold = null, bool? wordConfidence = null, bool? timestamps = null, bool? profanityFilter = null, bool? smartFormatting = null, bool? speakerLabels = null, string customizationId = null, string grammarName = null, bool? redaction = null, bool? processingMetrics = null, float? processingMetricsInterval = null, bool? audioMetrics = null, double? endOfPhraseSilenceTime = null, bool? splitTranscriptAtPhraseEnd = null, float? speechDetectorSensitivity = null, float? backgroundAudioSuppression = null)
+        public WebSocketClient RecognizeUsingWebSocket(RecognizeCallback callback, System.IO.MemoryStream audio, string contentType = null, string model = null, string languageCustomizationId = null, string acousticCustomizationId = null, string baseModelVersion = null, double? customizationWeight = null, long? inactivityTimeout = null, bool? interimResults = null, List<string> keywords = null, float? keywordsThreshold = null, long? maxAlternatives = null, float? wordAlternativesThreshold = null, bool? wordConfidence = null, bool? timestamps = null, bool? profanityFilter = null, bool? smartFormatting = null, bool? speakerLabels = null, string grammarName = null, bool? redaction = null, bool? processingMetrics = null, float? processingMetricsInterval = null, bool? audioMetrics = null, double? endOfPhraseSilenceTime = null, bool? splitTranscriptAtPhraseEnd = null, float? speechDetectorSensitivity = null, float? backgroundAudioSuppression = null)
         {
             if (callback == null)
             {
@@ -32,7 +32,7 @@ namespace IBM.Watson.SpeechToText.v1
                 {
                     webSocketClient.AddArgument("model", model);
                 }
-                if (!string.IsNullOrEmpty(customizationId))
+                if (!string.IsNullOrEmpty(languageCustomizationId))
                 {
                     webSocketClient.AddArgument("language_customization_id", languageCustomizationId);
                 }
@@ -44,102 +44,98 @@ namespace IBM.Watson.SpeechToText.v1
                 {
                     webSocketClient.AddArgument("base_model_version", baseModelVersion);
                 }
-                if(!string.IsNullOrEmpty(customizationId))
-                {
-                    webSocketClient.AddArgument("customization_id", customizationId);
-                }
 
                 // Websocket Open Message
                 if (!string.IsNullOrEmpty(contentType))
                 {
-                    webSocketClient.AddWebsocketParameter("content-type", contentType);
+                    webSocketClient.AddWebSocketParameter("content-type", contentType);
                 }
                 if (customizationWeight != null)
                 {
-                    webSocketClient.AddWebsocketParameter("customization_weight", customizationWeight);
+                    webSocketClient.AddWebSocketParameter("customization_weight", customizationWeight);
                 }
                 if (inactivityTimeout != null)
                 {
-                    webSocketClient.AddWebsocketParameter("inactivity_timeout", inactivityTimeout);
+                    webSocketClient.AddWebSocketParameter("inactivity_timeout", inactivityTimeout);
                 }
                 if (interimResults != null)
                 {
-                    webSocketClient.AddWebsocketParameter("interim_results", interimResults);
+                    webSocketClient.AddWebSocketParameter("interim_results", interimResults);
                 }
                 if (keywords != null && keywords.Count > 0)
                 {
-                    webSocketClient.AddWebsocketParameter("keywords", string.Join(",", keywords.Select(x => "\"" + x + "\"")));
+                    webSocketClient.AddWebSocketParameter("keywords", string.Join(",", keywords.Select(x => "\"" + x + "\"")));
                 }
                 if (keywordsThreshold != null)
                 {
-                    webSocketClient.AddWebsocketParameter("keywords_threshold", keywordsThreshold);
+                    webSocketClient.AddWebSocketParameter("keywords_threshold", keywordsThreshold);
                 }
                 if (maxAlternatives != null)
                 {
-                    webSocketClient.AddWebsocketParameter("max_alternatives", maxAlternatives);
+                    webSocketClient.AddWebSocketParameter("max_alternatives", maxAlternatives);
                 }
                 if (wordAlternativesThreshold != null)
                 {
-                    webSocketClient.AddWebsocketParameter("word_alternatives_threshold", wordAlternativesThreshold);
+                    webSocketClient.AddWebSocketParameter("word_alternatives_threshold", wordAlternativesThreshold);
                 }
                 if (wordConfidence != null)
                 {
-                    webSocketClient.AddWebsocketParameter("word_confidence", wordConfidence);
+                    webSocketClient.AddWebSocketParameter("word_confidence", wordConfidence);
                 }
                 if (timestamps != null)
                 {
-                    webSocketClient.AddWebsocketParameter("timestamps", timestamps);
+                    webSocketClient.AddWebSocketParameter("timestamps", timestamps);
                 }
                 if (profanityFilter != null)
                 {
-                    webSocketClient.AddWebsocketParameter("profanity_filter", profanityFilter);
+                    webSocketClient.AddWebSocketParameter("profanity_filter", profanityFilter);
                 }
                 if (smartFormatting != null)
                 {
-                    webSocketClient.AddWebsocketParameter("smart_formatting", smartFormatting);
+                    webSocketClient.AddWebSocketParameter("smart_formatting", smartFormatting);
                 }
                 if (speakerLabels != null)
                 {
-                    webSocketClient.AddWebsocketParameter("speaker_labels", speakerLabels);
+                    webSocketClient.AddWebSocketParameter("speaker_labels", speakerLabels);
                 }
                 if (!string.IsNullOrEmpty(grammarName))
                 {
-                    webSocketClient.AddWebsocketParameter("grammar_name", grammarName);
+                    webSocketClient.AddWebSocketParameter("grammar_name", grammarName);
                 }
                 if (redaction != null)
                 {
-                    webSocketClient.AddWebsocketParameter("redaction", redaction);
+                    webSocketClient.AddWebSocketParameter("redaction", redaction);
                 }
                 if (processingMetrics != null)
                 {
-                    webSocketClient.AddWebsocketParameter("processing_metrics", processingMetrics);
+                    webSocketClient.AddWebSocketParameter("processing_metrics", processingMetrics);
                 }
                 if (processingMetricsInterval != null)
                 {
-                    webSocketClient.AddWebsocketParameter("processing_metrics_interval", processingMetricsInterval);
+                    webSocketClient.AddWebSocketParameter("processing_metrics_interval", processingMetricsInterval);
                 }
                 if (audioMetrics != null)
                 {
-                    webSocketClient.AddWebsocketParameter("audio_metrics", audioMetrics);
+                    webSocketClient.AddWebSocketParameter("audio_metrics", audioMetrics);
                 }
                 if (endOfPhraseSilenceTime != null)
                 {
-                    webSocketClient.AddWebsocketParameter("end_of_phrase_silence_time", endOfPhraseSilenceTime);
+                    webSocketClient.AddWebSocketParameter("end_of_phrase_silence_time", endOfPhraseSilenceTime);
                 }
                 if (splitTranscriptAtPhraseEnd != null)
                 {
-                    webSocketClient.AddWebsocketParameter("split_transcript_at_phrase_end", splitTranscriptAtPhraseEnd);
+                    webSocketClient.AddWebSocketParameter("split_transcript_at_phrase_end", splitTranscriptAtPhraseEnd);
                 }
                 if (speechDetectorSensitivity != null)
                 {
-                    webSocketClient.AddWebsocketParameter("speech_detector_sensitivity", speechDetectorSensitivity);
+                    webSocketClient.AddWebSocketParameter("speech_detector_sensitivity", speechDetectorSensitivity);
                 }
                 if (backgroundAudioSuppression != null)
                 {
-                    webSocketClient.AddWebsocketParameter("background_audio_suppression", backgroundAudioSuppression);
+                    webSocketClient.AddWebSocketParameter("background_audio_suppression", backgroundAudioSuppression);
                 }
 
-                var sdkHeaders = Common.GetSdkHeaders("speech_to_text", "v1", "RecognizeUsingWebsockets");
+                var sdkHeaders = Common.GetSdkHeaders("speech_to_text", "v1", "RecognizeUsingWebSocket");
                 foreach (var header in sdkHeaders)
                 {
                     webSocketClient.WithHeader(header.Key, header.Value);

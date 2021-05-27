f40026dc8a00cf9cfad79833187b57cec586d828
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-05-27 14:57:56 -0500

    chore(common): update version

--- src/IBM.Watson.Common/Common.cs
+++ src/IBM.Watson.Common/Common.cs
@@ -27,7 +27,7 @@ namespace IBM.Watson
         /// <summary>
         /// The SDK version.
         /// </summary>
-        public const string Version = "watson-apis-dotnet-standard-sdk-5.1.0";
+        public const string Version = "watson-apis-dotnet-standard-sdk-5.2.0";
         private static string os;
         private static string osVersion;
         private static string frameworkDescription;

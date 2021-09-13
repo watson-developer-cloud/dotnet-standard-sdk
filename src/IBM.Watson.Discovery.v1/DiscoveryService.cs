c39d890d819a9d08bb511249ae00541d5621d52d
Nanwarin Chantarutai <nanwarin@ibm.com>
2021-09-12 23:56:18 -0700

    fix(discovery-v1): update status from string to StatusDetails

--- src/IBM.Watson.Discovery.v1/DiscoveryService.cs
+++ src/IBM.Watson.Discovery.v1/DiscoveryService.cs
@@ -1,5 +1,5 @@
 /**
-* (C) Copyright IBM Corp. 2017, 2021.
+* (C) Copyright IBM Corp. 2021.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 */
 
 /**
-* IBM OpenAPI SDK Code Generator Version: 3.38.0-07189efd-20210827-205025
+* IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-07189efd-20210907-091205
 */
  
 using System.Collections.Generic;
@@ -4566,12 +4566,10 @@ namespace IBM.Watson.Discovery.v1
         /// <param name="credentialDetails">Object containing details of the stored credentials.
         ///
         /// Obtain credentials for your source from the administrator of the source. (optional)</param>
-        /// <param name="status">The current status of this set of credentials. `connected` indicates that the
-        /// credentials are available to use with the source configuration of a collection. `invalid` refers to the
-        /// credentials (for example, the password provided has expired) and must be corrected before they can be used
-        /// with a collection. (optional)</param>
+        /// <param name="status">Object that contains details about the status of the authentication process.
+        /// (optional)</param>
         /// <returns><see cref="Credentials" />Credentials</returns>
-        public DetailedResponse<Credentials> CreateCredentials(string environmentId, string sourceType = null, CredentialDetails credentialDetails = null, string status = null)
+        public DetailedResponse<Credentials> CreateCredentials(string environmentId, string sourceType = null, CredentialDetails credentialDetails = null, StatusDetails status = null)
         {
             if (string.IsNullOrEmpty(Version))
             {
@@ -4610,9 +4608,9 @@ namespace IBM.Watson.Discovery.v1
                 {
                     bodyObject["credential_details"] = JToken.FromObject(credentialDetails);
                 }
-                if (!string.IsNullOrEmpty(status))
+                if (status != null)
                 {
-                    bodyObject["status"] = status;
+                    bodyObject["status"] = JToken.FromObject(status);
                 }
                 var httpContent = new StringContent(JsonConvert.SerializeObject(bodyObject), Encoding.UTF8, HttpMediaType.APPLICATION_JSON);
                 restRequest.WithBodyContent(httpContent);
@@ -4720,12 +4718,10 @@ namespace IBM.Watson.Discovery.v1
         /// <param name="credentialDetails">Object containing details of the stored credentials.
         ///
         /// Obtain credentials for your source from the administrator of the source. (optional)</param>
-        /// <param name="status">The current status of this set of credentials. `connected` indicates that the
-        /// credentials are available to use with the source configuration of a collection. `invalid` refers to the
-        /// credentials (for example, the password provided has expired) and must be corrected before they can be used
-        /// with a collection. (optional)</param>
+        /// <param name="status">Object that contains details about the status of the authentication process.
+        /// (optional)</param>
         /// <returns><see cref="Credentials" />Credentials</returns>
-        public DetailedResponse<Credentials> UpdateCredentials(string environmentId, string credentialId, string sourceType = null, CredentialDetails credentialDetails = null, string status = null)
+        public DetailedResponse<Credentials> UpdateCredentials(string environmentId, string credentialId, string sourceType = null, CredentialDetails credentialDetails = null, StatusDetails status = null)
         {
             if (string.IsNullOrEmpty(Version))
             {
@@ -4772,9 +4768,9 @@ namespace IBM.Watson.Discovery.v1
                 {
                     bodyObject["credential_details"] = JToken.FromObject(credentialDetails);
                 }
-                if (!string.IsNullOrEmpty(status))
+                if (status != null)
                 {
-                    bodyObject["status"] = status;
+                    bodyObject["status"] = JToken.FromObject(status);
                 }
                 var httpContent = new StringContent(JsonConvert.SerializeObject(bodyObject), Encoding.UTF8, HttpMediaType.APPLICATION_JSON);
                 restRequest.WithBodyContent(httpContent);

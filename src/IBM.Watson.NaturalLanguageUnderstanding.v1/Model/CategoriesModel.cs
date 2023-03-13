8631969c3ffe9193f5f6119cf6978d6246bb739d
Angelo Paparazzi <angelo.paparazzi@ibm.com>
2023-03-13 13:52:48 -0500

    fix(nlu): hand edits

--- src/IBM.Watson.NaturalLanguageUnderstanding.v1/Model/CategoriesModel.cs
+++ src/IBM.Watson.NaturalLanguageUnderstanding.v1/Model/CategoriesModel.cs
@@ -73,7 +73,7 @@ namespace IBM.Watson.NaturalLanguageUnderstanding.v1.Model
         /// An optional map of metadata key-value pairs to store with this model.
         /// </summary>
         [JsonProperty("user_metadata", NullValueHandling = NullValueHandling.Ignore)]
-        public Dictionary<string, Dictionary> UserMetadata { get; set; }
+        public Dictionary<string, object> UserMetadata { get; set; }
         /// <summary>
         /// The 2-letter language code of this model.
         /// </summary>
